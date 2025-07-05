a416eb96b7626173facb4e75ce9cbded43945318
Bowen <hwbrzzl@gmail.com>
2025-07-05 10:37:24 +0800

    test

--- setup/setup.go
+++ setup/setup.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"os"
 
 	"github.com/goravel/framework/packages"
 	"github.com/goravel/framework/packages/match"
 	"github.com/goravel/framework/packages/modify"
-	supportfile "github.com/goravel/framework/support/file"
 	"github.com/goravel/framework/support/path"
 )
 
+//go:embed config/hello.go
+var files embed.FS
+
 func main() {
-	config, err := supportfile.GetContent("config/hello.go")
+	config, err := fs.ReadFile(files, "config/hello.go")
 	if err != nil {
 		panic(err)
 	}
@@ -21,7 +25,7 @@ func main() {
 			modify.GoFile(path.Config("app.go")).
 				Find(match.Imports()).Modify(modify.AddImport(packages.GetModulePath(), "examplepackage")).
 				Find(match.Providers()).Modify(modify.Register("&examplepackage.ServiceProvider{}")),
-			modify.File(path.Config("hello.go")).Overwrite(config),
+			modify.File(path.Config("hello.go")).Overwrite(string(config)),
 		).
 		Uninstall(
 			modify.GoFile(path.Config("app.go")).
