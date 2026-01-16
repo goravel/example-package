429ce19dee815fc35ecc9d3879384d269a26f6a6
Wenbo Han <hwbrzzl@gmail.com>
2026-01-16 17:15:02 +0800

    feat: optimize setup (#5)
    * feat: optimize setup
    
    * optimize

--- setup/setup.go
+++ setup/setup.go
@@ -4,32 +4,35 @@ import (
 	"os"
 
 	"github.com/goravel/framework/packages"
-	"github.com/goravel/framework/packages/match"
 	"github.com/goravel/framework/packages/modify"
-	supportfile "github.com/goravel/framework/support/file"
+	"github.com/goravel/framework/support/file"
 	"github.com/goravel/framework/support/path"
 )
 
 func main() {
-	// The config file will be published to the project's config directory when installing by this way.
+	setup := packages.Setup(os.Args)
+
+	// The config file will be published to the project's config directory automatically when installing by this way.
 	// You can also publish this config file manually: ./artisan vendor:publish --package=github.com/goravel/example-package
-	config, err := supportfile.GetPackageContent(packages.GetModulePath(), "setup/config/hello.go")
+	config, err := file.GetPackageContent(setup.Paths().Module().String(), "setup/config/hello.go")
 	if err != nil {
 		panic(err)
 	}
 
-	packages.Setup(os.Args).
-		Install(
-			modify.GoFile(path.Config("app.go")).
-				Find(match.Imports()).Modify(modify.AddImport(packages.GetModulePath(), "examplepackage")).
-				Find(match.Providers()).Modify(modify.Register("&examplepackage.ServiceProvider{}")),
-			modify.File(path.Config("hello.go")).Overwrite(config),
-		).
-		Uninstall(
-			modify.GoFile(path.Config("app.go")).
-				Find(match.Providers()).Modify(modify.Unregister("&examplepackage.ServiceProvider{}")).
-				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath(), "examplepackage")),
-			modify.File(path.Config("hello.go")).Remove(),
-		).
-		Execute()
+	serviceProvider := "&hello.ServiceProvider{}"
+	moduleImport := setup.Paths().Module().Import()
+
+	setup.Install(
+		// Add the service provider to the providers slice in bootstrap/providers.go
+		modify.AddProviderApply(moduleImport, serviceProvider),
+
+		// Add the config file to the config directory
+		modify.File(path.Config("hello.go")).Overwrite(config),
+	).Uninstall(
+		// Remove the config file from the config directory
+		modify.File(path.Config("hello.go")).Remove(),
+
+		// Remove the service provider from the providers slice in bootstrap/providers.go
+		modify.RemoveProviderApply(moduleImport, serviceProvider),
+	).Execute()
 }
