26afd014bec01390db80c5050afb9105c93ce4a2
Bowen <hwbrzzl@gmail.com>
2026-01-16 17:00:58 +0800

    feat: optimize setup

--- setup/setup.go
+++ setup/setup.go
@@ -4,32 +4,31 @@ import (
 	"os"
 
 	"github.com/goravel/framework/packages"
-	"github.com/goravel/framework/packages/match"
 	"github.com/goravel/framework/packages/modify"
 	supportfile "github.com/goravel/framework/support/file"
 	"github.com/goravel/framework/support/path"
 )
 
 func main() {
+	setup := packages.Setup(os.Args)
+
 	// The config file will be published to the project's config directory when installing by this way.
 	// You can also publish this config file manually: ./artisan vendor:publish --package=github.com/goravel/example-package
-	config, err := supportfile.GetPackageContent(packages.GetModulePath(), "setup/config/hello.go")
+	config, err := supportfile.GetPackageContent(setup.Paths().Module().String(), "setup/config/hello.go")
 	if err != nil {
 		panic(err)
 	}
 
-	packages.Setup(os.Args).
-		Install(
-			modify.GoFile(path.Config("app.go")).
-				Find(match.Imports()).Modify(modify.AddImport(packages.GetModulePath(), "examplepackage")).
-				Find(match.Providers()).Modify(modify.Register("&examplepackage.ServiceProvider{}")),
-			modify.File(path.Config("hello.go")).Overwrite(config),
-		).
-		Uninstall(
-			modify.GoFile(path.Config("app.go")).
-				Find(match.Providers()).Modify(modify.Unregister("&examplepackage.ServiceProvider{}")).
-				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath(), "examplepackage")),
-			modify.File(path.Config("hello.go")).Remove(),
-		).
-		Execute()
+	serviceProvider := "&hello.ServiceProvider{}"
+	moduleImport := setup.Paths().Module().Import()
+
+	setup.Install(
+		modify.AddProviderApply(moduleImport, serviceProvider),
+
+		modify.File(path.Config("hello.go")).Overwrite(config),
+	).Uninstall(
+		modify.File(path.Config("hello.go")).Remove(),
+
+		modify.RemoveProviderApply(moduleImport, serviceProvider),
+	).Execute()
 }
